035e3963e29b1d4d1a7ce72e1ed76aef255d158f
deatiger <deatiger1492@gmail.com>
2020-05-03 11:03:55 +0900

    Added promise.js file

--- asynchronous.js
+++ asynchronous.js
@@ -2,23 +2,20 @@ import fetch from 'node-fetch';
 
 // 非同期処理をおこなう関数を宣言
 const getGitUsername = () => {
-    return new Promise((resolve, reject) => {
-        const url = 'https://api.github.com/users/deatiger'
+    const url = 'https://api.github.com/users/deatiger'
 
-        // GitHub APIをFetchメソッドで実行
-        fetch(url).then(res => res.json())
-            .then(json => {
-                console.log('これは非同期処理成功時のメッセージです')
-                return resolve(json.login)
-            }).catch(error => {
-                console.error('これは非同期処理失敗時のメッセージです。', error)
-                return reject(null)
-            })
+    // GitHub APIをFetchメソッドで実行
+    fetch(url).then(res => res.json())
+        .then(json => {
+            console.log('これは非同期処理成功時のメッセージです')
+            return json.login
+        }).catch(error => {
+            console.error('これは非同期処理失敗時のメッセージです。', error)
+            return null
+        })
 
-    })
 };
 
 const message = 'GitのユーザーIDは'
-getGitUsername().then(username => {
-    console.log(message + username)
-});
+const username = getGitUsername()
+console.log(message + username)
